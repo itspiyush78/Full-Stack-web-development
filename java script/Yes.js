d39288cb34ce37585d1a983e02de0dd690427e02
piyush singh <123636271+itspiyush78@users.noreply.github.com>
2023-07-30 09:22:09 +0530

    Basics

---
+++ java script/Yes.js
@@ -0,0 +1,62 @@
+
+// Values
+
+// 7 = > int
+// 8.1 = > float
+// Number
+
+
+// 'piyush'
+// "piyush"
+// String
+
+// true or false
+// Boolean values
+
+// Null
+// undefined 
+
+// Primitive Datatype
+// Single value
+
+// Non - Primitive value = > Multi values
+// arrays = > [] = > index
+//objects = > {} = > key :Value
+// ["A", "B", "C", "D", "E", "F"];
+
+
+// Variables
+
+// var
+// let
+// const
+
+//let number = 2510;
+
+//let number2 = 34;
+//console.log(number);
+
+//console.log(number2);
+
+// Assignment operator
+let a =  50;
+let b = 25;
+let c = 50;
+
+// Arithmetic operator
+
+//console.log(a + b);
+//console.log(a - b);
+//console.log(a % b);
+//console.log(a *b);
+//console.log(a / b);
+
+//console.log(3 ** 3);
+
+// Comparison Operators
+
+// = =
+// = = =
+console.log(a >= b);
+
+
