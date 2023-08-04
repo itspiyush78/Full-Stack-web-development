0f62b5c2cf04d0c9afedfc46bb181b1954be0d0d
piyush singh <123636271+itspiyush78@users.noreply.github.com>
2023-08-04 13:01:02 +0530

    function

---
+++ java script/Jsfundamentals1/function.js
@@ -0,0 +1,17 @@
+function myFirstfunction() {
+    // all the logic inside the pair of curly braces
+    console.log("This is");
+    console.log("My first function");
+}
+
+function isEvenOrodd(x) { // it takes a value  x and tells whether it is even or odd ?
+    // x -> parameter 
+    if(x % 2 == 0) {
+        console.log("Even");
+} else {
+    console.log("Odd");
+}
+
+
+isEvenOrodd(12);   // 12 -> arguments
+}
